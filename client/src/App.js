ae2b0f4f92eb93e4e4108fe4f1b26c1d63006edc
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2021-07-19 19:14:46 +0300

    added Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AllPosts from "./Components/Posts/AllPosts.component"
+import Login from "./Components/Auth/Login";
 
 
 const App = () => {
+
   return (
     <Router>
       <div>
@@ -12,6 +15,10 @@ const App = () => {
        {/* <Route
           path='/register'
           component={Register} /> */}
+          <Route
+            exact path='/login'
+            component={Login}
+          />
       </div>
     </Router>
   );
