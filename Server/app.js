92d819573e2656d3d82298b13dc6d0918488f364
Vlad-Vidican <vlad.vidican00@e-uvt.ro>
2021-07-19 12:18:27 +0300

    Implemented company authentication

--- Server/app.js
+++ Server/app.js
@@ -11,6 +11,7 @@ const auth = require("./middleware/auth");
 const User = require("./models/user");
 const Post = require("./models/post");
 const Comment = require("./models/comment");
+const Company = require("./models/company.js");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,68 +28,42 @@ app.get("/registerUser", (req, res) => {
 	res.render("registerForm.ejs");
 });
 
-app.post("/registerUser", async (req, res) => {
-	try {
-		//Get user Inputs
-		const { email, firstName, lastName, password, role } = req.body;
-		// Validate data
-		console.log(req.body);
-		if (!(email && firstName && lastName && password && role)) {
-			res.status(400).send("All inputs are required");
-		}
+app.post("/register", async (req, res) => {
+    try {
 
-		// Check if user exist
-		const oldUser = await User.findOne({ email });
+		if(req.body.role === "Student")
+		{
+			console.log
+        //Get user Inputs
+			const { email, firstName, lastName, password, role } = req.body;
+			// Validate data
 
-		if (oldUser) {
-			return res.status(409).send("User already exist!");
-		}
-
-		// encrypt password
-		encryptedPassword = await bcrypt.hash(password, 10);
-
-		//reate user to db
-
-		const user = await User.create({
-			email: email.toLowerCase(),
-			firstName,
-			lastName,
-			password: encryptedPassword,
-			role,
-		});
-
-		// Create token
-		const token = jwt.sign(
-			{ user_id: user._id, email },
-			process.env.TOKEN_KEY,
-			{
-				expiresIn: "2h",
+			if (!(email && firstName && lastName && password && role)) {
+				res.status(400).send("All inputs are required");
 			}
-		);
-		user.token = token;
-	} catch (err) {
-		console.log(err);
-	}
-});
 
-//LOGIN
+			// Check if user exist
+			const oldUser = await User.findOne({ email });
+			const oldCompany = await Company.findOne({email});
 
-app.get("/loginUser", (req, res) => {
-	res.render("loginForm.ejs");
-});
+			if (oldUser || oldCompany) {
+				return res.status(409).send("email is already used for an account");
+			}
 
-app.post("/loginUser", async (req, res) => {
-	try {
-		//Get user data
-		const { email, password } = req.body;
-		//Validate data
-		if (!(email && password)) {
-			res.status(400).send("All inputs are required");
-		}
-		//Verify if user exist
-		const user = await User.findOne({ email });
+			// encrypt password
+			encryptedPassword = await bcrypt.hash(password, 10);
 
-		if (user && (await bcrypt.compare(password, user.password))) {
+			//reate user to db
+
+			const user = await User.create({
+				email: email.toLowerCase(),
+				firstName,
+				lastName,
+				password: encryptedPassword,
+				role,
+			});
+
+			// Create token
 			const token = jwt.sign(
 				{ user_id: user._id, email },
 				process.env.TOKEN_KEY,
@@ -96,15 +71,99 @@ app.post("/loginUser", async (req, res) => {
 					expiresIn: "2h",
 				}
 			);
-			// save token
 			user.token = token;
-			//user
-			res.status(200).json(user);
+			return res.send("User created!");
 		}
-		res.status(400).send("Invalid email or password");
-	} catch (err) {
-		console.log(err);
-	}
+		else if(req.body.role === 'Company')
+		{
+			const {email,password,companyName,role} = req.body;
+
+			if(!(email && companyName && password && role))
+			{
+				res.status(400).send("All inputs are required");
+			}
+
+			const oldUser = await User.findOne({ email });
+			const oldCompany = await Company.findOne({email});
+
+			if (oldUser || oldCompany) {
+				return res.status(409).send("email is already used for an account");
+			}
+			encryptedPassword = await bcrypt.hash(password,10)
+
+			const company = await Company.create({
+				email: email.toLowerCase(),
+				password: encryptedPassword,
+				companyName,
+				role,
+			})
+			const token = jwt.sign(
+				{ comapny_id: company._id, email },
+				process.env.TOKEN_KEY,
+				{
+					expiresIn: "2h",
+				}
+			);
+			company.token = token;
+			return res.send("Company account created!");
+		}
+    } catch (err) {
+        console.log(err);
+
+    }
+});
+
+//LOGIN
+
+app.get("/loginUser", (req, res) => {
+    res.render("loginForm.ejs");
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        //Get user data
+        const { email, password } = req.body;
+        //Validate data
+        if (!(email && password)) {
+            res.status(400).send("All inputs are required");
+        }
+        //Verify if user exist
+        const user = await User.findOne({ email });
+		const company = await Company.findOne({email});
+
+		
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                }
+            );
+            // save token
+            user.token = token;
+            //user
+            res.status(200).json(user);
+        }
+		else if(company && (await bcrypt.compare(password,company.password)))
+		{
+			const token = jwt.sign(
+				{company_id: company._id,email},
+				process.env.TOKEN_KEY,
+				{
+					expiresIn: "2h",
+				}
+			);
+			company.token = token;
+			res.status(200).json(company);
+		}
+		else
+			{
+				res.status(400).send("Invalid email or password");
+			}
+    } catch (err) {
+        console.log(err);
+    }
 });
 app.post("/welcome", auth, (req, res) => {
 	res.status(200).send("Welcome 🙌 ");
