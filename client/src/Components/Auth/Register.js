ffdc1f933a6850ea0c949cf5f4c466cb32cc039c
AdelinCraciunesc <lascu.craciunesc00@e-uvt.ro>
2021-07-27 19:15:18 +0300

    dynamic inputs for register

--- client/src/Components/Auth/Register.js
+++ client/src/Components/Auth/Register.js
@@ -4,20 +4,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope,faLock,faUser } from '@fortawesome/free-solid-svg-icons';
 
 
-function Register() {
+const Register = ({}) => {
+    const [isStudent,setState] = React.useState(true);
+
+    const handleClick = (e) => {
+        if(e.target.value == "Student")
+        {
+            setState(true)
+        }
+        else
+        {
+            setState(false);
+        }
+    };
+
     return (
         <Body>
             <Container>
                 <RegisterForm>
                     <H1Reg>Sign up</H1Reg>
-                    <InputContainer>
-                        <LabelReg><FontAwesomeIcon icon={ faUser } className="icon" fixedWidth /></LabelReg>
-                        <Div><Input type="text" name="firstname" id="firstname" placeholder="First Name"/></Div>
-                    </InputContainer>
-                    <InputContainer>
-                        <LabelReg><FontAwesomeIcon icon={ faUser } className="icon" fixedWidth /></LabelReg>
-                        <Div><Input type="text" name="lastname" id="lastname" placeholder="Last Name"/></Div>
-                    </InputContainer>
+                    {isStudent == true &&
+                        <InputContainer>
+                            <LabelReg><FontAwesomeIcon icon={ faUser } className="icon" fixedWidth /></LabelReg>
+                            <Div><Input type="text" name="firstname" id="firstname" placeholder="First Name"/></Div>
+                        </InputContainer>
+                    }
+                    {isStudent == true &&
+                        <InputContainer>
+                            <LabelReg><FontAwesomeIcon icon={ faUser } className="icon" fixedWidth /></LabelReg>
+                            <Div><Input type="text" name="lastname" id="lastname" placeholder="Last Name"/></Div>
+                        </InputContainer>
+                    }
+                    {isStudent == false &&
+                        <InputContainer>
+                            <LabelReg><FontAwesomeIcon icon={ faUser } className="icon" fixedWidth /></LabelReg>
+                            <Div><Input type="text" name="lastname" id="lastname" placeholder="Company name"/></Div>
+                        </InputContainer>
+                    }
                     <InputContainer>
                         <LabelReg><FontAwesomeIcon icon={ faEnvelope } className="icon" fixedWidth /></LabelReg>
                         <Div><Input type="text" name="email" id="email" placeholder="Email"/></Div>
@@ -27,7 +50,7 @@ function Register() {
                         <Div><Input type="password" name="password" id="password" placeholder="Password"/></Div>
                     </InputContainer>
                     <DropdownContainer>
-                        <Dropdown required>
+                        <Dropdown required onChange={handleClick}>
                             <option value="" disabled selected hidden>Select your role</option>
                             <option value="Student">Student</option>
                             <option value="Company">Company</option>
